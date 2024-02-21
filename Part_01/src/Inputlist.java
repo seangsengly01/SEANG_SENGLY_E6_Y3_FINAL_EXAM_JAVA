6b70dd8130bb3ab60180267856dd620188982e39
seangsengly <demoemail1402@gmail.com>
2024-02-21 18:16:31 +0700

    Inputview part

---
+++ Part_01/src/Inputlist.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Inputlist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid number of elements.");
+            return;
+        }
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.println("The input numbers are:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
